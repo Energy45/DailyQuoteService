5853d756ab1dc2c4a65ce6c626b7d689e508e088
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2021-02-11 10:04:50 +0100

    Méthode getPageContents

--- src/main/java/com/ipiecoles/dailyquote/DailyQuote.java
+++ src/main/java/com/ipiecoles/dailyquote/DailyQuote.java
@@ -1,8 +1,33 @@
 package com.ipiecoles.dailyquote;
 
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.StringJoiner;
+
 public class DailyQuote {
 
     public DailyQuoteData getDailyQuote() {
         return null;
+    }
+
+    private String getPageContents(String address) throws IOException {
+        BufferedReader br = null;
+        StringJoiner lines = new StringJoiner(System.lineSeparator());
+        try {
+            URL url = new URL(address);
+            br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } finally {
+            if (br != null) {
+                br.close();
+            }
+        }
+        return lines.toString();
+
     }
 }
