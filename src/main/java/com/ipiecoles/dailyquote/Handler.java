3fc093741508d3df3dd296e059e5dd41e4022a43
PierrePocheron <pierre.pocheron@gmail.com>
2021-02-12 09:31:21 +0100

    Update Handler.java

--- src/main/java/com/ipiecoles/dailyquote/Handler.java
+++ src/main/java/com/ipiecoles/dailyquote/Handler.java
@@ -4,6 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.owlike.genson.Genson;
 import com.owlike.genson.GensonBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -13,13 +16,15 @@ public class Handler implements RequestHandler<Object, GatewayResponse> {
     @Override
     public GatewayResponse handleRequest(Object input, Context context) {
 
-        Ephemeride ephemeride = new Ephemeride();
+        DailyQuote dailyQuote = new DailyQuote();
         try {
             //Appel ma méthode métier
-            EphemerideData ephemerideData = ephemeride.getEphemerideFromDate(LocalDate.now());
+            DailyQuoteData dailyQuoteData = dailyQuote.getDailyQuote();
+
             //Serialize en JSON l'objet de retour
             Genson genson = new GensonBuilder().useRuntimeType(true).create();
-            String body = genson.serialize(ephemerideData);
+            String body = genson.serialize(dailyQuoteData);
+
             //Gère les headers
             Map<String, String> headers = new HashMap<>();
             headers.put("Content-Type", "application/json");
@@ -27,7 +32,7 @@ public class Handler implements RequestHandler<Object, GatewayResponse> {
             int statusCode = 200;
             //Renvoie de l'objet à API Gateway
             return new GatewayResponse(body, headers, statusCode);
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             //Gère correctement l'exception
             e.printStackTrace();
         }
