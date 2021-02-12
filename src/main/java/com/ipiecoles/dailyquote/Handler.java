d340d595d7c891fa79c818341741cdbf198b7add
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2021-02-12 09:37:46 +0100

    Update Handler class

--- src/main/java/com/ipiecoles/dailyquote/Handler.java
+++ src/main/java/com/ipiecoles/dailyquote/Handler.java
@@ -2,13 +2,10 @@ package com.ipiecoles.dailyquote;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.owlike.genson.Genson;
-import com.owlike.genson.GensonBuilder;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,11 +17,12 @@ public class Handler implements RequestHandler<Object, GatewayResponse> {
         try {
             //Appel ma méthode métier
             DailyQuoteData dailyQuoteData = dailyQuote.getDailyQuote();
-
             //Serialize en JSON l'objet de retour
-            Genson genson = new GensonBuilder().useRuntimeType(true).create();
-            String body = genson.serialize(dailyQuoteData);
+            JSONObject root = new JSONObject();
+            root.put("author", dailyQuoteData.getAuthor());
+            root.put("quote", dailyQuoteData.getQuote());
 
+            String body = root.toJSONString();
             //Gère les headers
             Map<String, String> headers = new HashMap<>();
             headers.put("Content-Type", "application/json");
@@ -32,7 +30,7 @@ public class Handler implements RequestHandler<Object, GatewayResponse> {
             int statusCode = 200;
             //Renvoie de l'objet à API Gateway
             return new GatewayResponse(body, headers, statusCode);
-        } catch (IOException | ParseException e) {
+        } catch (ParseException e) {
             //Gère correctement l'exception
             e.printStackTrace();
         }
