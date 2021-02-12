1f983a92614bab20e40fe162cc3b3a5a52a40789
PierrePocheron <pierre.pocheron@gmail.com>
2021-02-12 09:22:56 +0100

    Update Handler.java

--- src/main/java/com/ipiecoles/dailyquote/Handler.java
+++ src/main/java/com/ipiecoles/dailyquote/Handler.java
@@ -1,30 +1,37 @@
 package com.ipiecoles.dailyquote;
 
-import com.sun.tools.sjavac.server.RequestHandler;
-
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.owlike.genson.Genson;
+import com.owlike.genson.GensonBuilder;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Handler implements RequestHandler<Object, GatewayResponse> {
     @Override
-    public GatewayResponse handlerRequest(Object input, Context context) {
+    public GatewayResponse handleRequest(Object input, Context context) {
 
-        DailyQuote dailyQuote = new DailyQuote();
-        try{
-            dailyQuote.getDailyQuoteFromDate(LocalDate.now());
-            String body;
+        Ephemeride ephemeride = new Ephemeride();
+        try {
+            //Appel ma méthode métier
+            EphemerideData ephemerideData = ephemeride.getEphemerideFromDate(LocalDate.now());
+            //Serialize en JSON l'objet de retour
+            Genson genson = new GensonBuilder().useRuntimeType(true).create();
+            String body = genson.serialize(ephemerideData);
+            //Gère les headers
             Map<String, String> headers = new HashMap<>();
             headers.put("Content-Type", "application/json");
-            headers.put("Access-Control ", "application/json");
+            headers.put("Access-Control-Allow-Origin", "https://pjvilloud.github.io");
             int statusCode = 200;
-
-            GatewayResponse gatewayResponse = new GatewayResponse(body headers statusCode);
-            return  gatewayResponse;
+            //Renvoie de l'objet à API Gateway
+            return new GatewayResponse(body, headers, statusCode);
+        } catch (IOException e) {
+            //Gère correctement l'exception
+            e.printStackTrace();
         }
-        catch (Exception e){
 
-        }
-
+        return null;
     }
 }
